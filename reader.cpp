d640571b6e13eccc96e2db364e32c1c92212ff60
Efe Can İçöz <efecanicoz@gmail.com>
2016-07-11 09:53:39 +0300

    initial commit, i've started to work but forget about git. Lets use version tracker to make nothing harmful.

---
+++ reader.cpp
@@ -0,0 +1,163 @@
+#include "reader.h"
+#include <vector>
+
+const vector<uint8_t> ELF_H = {0x7F,0x45,0x4C,0x46};
+
+struct e_ident readElfIdent(ifstream& fd)
+{
+    struct e_ident id;
+    fd.read((char *)id.ei_magic,4);
+    fd.read((char *)&id.ei_class,1);
+    fd.read((char *)&id.ei_data,1);
+    fd.read((char *)&id.ei_version,1);
+    fd.read((char *)&id.ei_osabi,1);
+    fd.read((char *)&id.ei_abiversion,1);
+    fd.read((char *)id.ei_pad,7);
+    
+    return id;
+}
+
+void readLittleEndian(uint8_t *to, uint8_t from[], int offset)
+{
+    *to = from[offset];
+}
+void readLittleEndian(uint16_t *to, uint8_t from[], int offset)
+{
+    *to = from[offset] 
+        |from[offset+1]<<8;
+}
+void readLittleEndian(uint32_t *to, uint8_t from[], int offset)
+{
+    *to = from[offset]
+        |from[offset+1]<<8
+        |from[offset+2]<<16
+        |from[offset+3]<<24;
+}
+void readLittleEndian(uint64_t *to, uint8_t from[], int offset)
+{
+    *to = (static_cast<uint64_t>(from[offset]) <<  0) |
+        (static_cast<uint64_t>(from[offset+1]) <<  8) |
+        (static_cast<uint64_t>(from[offset+2]) << 16) |
+        (static_cast<uint64_t>(from[offset+3]) << 24) |
+        (static_cast<uint64_t>(from[offset+4]) << 32) |
+        (static_cast<uint64_t>(from[offset+5]) << 40) |
+        (static_cast<uint64_t>(from[offset+6]) << 48) |
+        (static_cast<uint64_t>(from[offset+7]) << 56) ; 
+}
+
+struct e64_header readElf64Header(ifstream& fd)
+{
+    struct e64_header header;
+    uint8_t buffer[48];
+    fd.read((char *)&buffer,48);
+    readLittleEndian(&(header.e_type),buffer,0);
+    readLittleEndian(&(header.e_machine), buffer,2);
+    readLittleEndian(&(header.e_version), buffer,4);
+    readLittleEndian(&(header.e_entry), buffer,8);
+    readLittleEndian(&(header.e_phoff), buffer, 16);
+    readLittleEndian(&(header.e_shoff), buffer, 24);
+    readLittleEndian(&(header.e_flags), buffer, 32);
+    readLittleEndian(&(header.e_ehsize), buffer, 36);
+    readLittleEndian(&(header.e_phentsize), buffer, 38);
+    readLittleEndian(&(header.e_phnum), buffer, 40);
+    readLittleEndian(&(header.e_shentsize), buffer, 42);
+    readLittleEndian(&(header.e_shnum), buffer, 44);
+    readLittleEndian(&(header.e_shstrndx), buffer, 46);
+    
+    //printf("%x   %x   %x   \n%x      %x   %x\n",header.e_phoff, header.e_phentsize,header.e_phnum,header.e_shoff,header.e_shentsize,header.e_shnum);
+    return header;
+}
+
+struct e32_section_header read32Section(ifstream& fd, uint32_t offset, uint16_t shstr_offset)
+{
+    fd.seekg(offset);
+    uint8_t buffer[40];
+    char c;
+    struct e32_section_header secHeader;
+    fd.read((char *)buffer, 40);
+    readLittleEndian(&(secHeader.sh_name), buffer, 0);
+    readLittleEndian(&(secHeader.sh_type), buffer, 4);
+    readLittleEndian(&(secHeader.sh_flags), buffer, 8);
+    readLittleEndian(&(secHeader.sh_addr), buffer, 12);
+    readLittleEndian(&(secHeader.sh_offset), buffer, 16);
+    readLittleEndian(&(secHeader.sh_size), buffer, 20);
+    readLittleEndian(&(secHeader.sh_link), buffer, 24);
+    readLittleEndian(&(secHeader.sh_info), buffer, 28);
+    readLittleEndian(&(secHeader.sh_addralign), buffer, 32);
+    readLittleEndian(&(secHeader.sh_entsize), buffer, 36);
+    
+    secHeader.name = "";
+    fd.seekg(secHeader.sh_name);
+    do
+    {
+        fd.read(&c, 1);
+        secHeader.name += c;
+    }while(c != '\0');
+
+    return secHeader;
+}
+
+void readElf32Header(ifstream& fd)
+{
+    struct e32_header header;
+/*    fd.read((char *)header.e_type,2);
+    fd.read((char *)header.e_machine,2); 
+    fd.read((char *)header.e_version,4); 
+    fd.read((char *)header.e_entry,4); 
+    fd.read((char *)header.e_phoff,4); 
+    fd.read((char *)header.e_shoff,4); 
+    fd.read((char *)header.e_flags,4); 
+    fd.read((char *)header.e_ehsize,2); 
+    fd.read((char *)header.e_phentsize,2); 
+    fd.read((char *)header.e_phnum,2); 
+    fd.read((char *)header.e_shentsize,2); 
+    fd.read((char *)header.e_shnum,2); 
+    fd.read((char *)header.e_shstrndx,2);
+*/
+    return;
+}
+
+void readElf(ifstream& fd)
+{
+    struct e_ident identifier = readElfIdent(fd);
+    if(identifier.ei_class == 1)
+        readElf32Header(fd);
+    else if(identifier.ei_class == 2)
+    {
+        uint64_t offset;
+        struct e64_header header;
+        header = readElf64Header(fd);
+        vector<struct e32_section_header> sHeaders;
+        offset = header.e_shoff;
+        for(int i=0; i < header.e_shnum; i++)
+        {
+            sHeaders.push_back(read32Section(fd, offset, header.e_shstrndx));
+            offset += header.e_shentsize;
+        }
+    }
+    else
+        printf("error");
+    
+    return;
+}
+
+string readFile(const string path)
+{
+    vector<uint8_t> buffer(4);
+    
+    ifstream fd(path, ios::in|ios::binary);
+
+    fd.read((char *)&buffer[0],4);
+    for(int i = 0; i < 4; i++)
+    {
+        printf("%x %x\n",buffer[i],ELF_H[i]);
+    }
+    fd.seekg(0);
+    if(buffer == ELF_H)
+        readElf(fd);
+    else
+        printf("üzüldük");
+    string a = "elf";
+
+    return a;
+}
---
+++ reader.h
@@ -0,0 +1,89 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <cstdio>
+#include <cstdint>
+
+using namespace std;
+
+struct e_ident
+{
+    uint8_t ei_magic[4];
+    uint8_t ei_class;
+    uint8_t ei_data;
+    uint8_t ei_version;
+    uint8_t ei_osabi;
+    uint8_t ei_abiversion;
+    uint8_t ei_pad[7];
+};
+
+struct e32_section_header
+{
+    uint32_t sh_name;
+    string   name;
+    uint32_t sh_type;
+    uint32_t sh_flags;
+    uint32_t sh_addr;
+    uint32_t sh_offset;
+    uint32_t sh_size;
+    uint32_t sh_link;
+    uint32_t sh_info;
+    uint32_t sh_addralign;
+    uint32_t sh_entsize;
+};
+
+struct e32_program_header
+{
+    uint32_t p_type;
+    uint32_t p_offset;
+    uint32_t p_vaddr;
+    uint32_t p_paddr;
+    uint32_t p_filesz;
+    uint32_t p_memsz;
+    uint32_t p_flags;
+    uint32_t p_align;
+};
+
+struct e32_header
+{
+    uint16_t e_type;
+    uint16_t e_machine;
+    uint32_t e_version;
+    uint32_t e_entry;
+    uint32_t e_phoff;
+    uint32_t e_shoff;
+    uint32_t e_flags;
+    uint16_t e_ehsize;
+    uint16_t e_phentsize;
+    uint16_t e_phnum;
+    uint16_t e_shentsize;
+    uint16_t e_shnum;
+    uint16_t e_shstrndx;
+};
+
+struct e64_header
+{
+    uint16_t e_type;
+    uint16_t e_machine;
+    uint32_t e_version;
+    uint64_t e_entry;
+    uint64_t e_phoff;
+    uint64_t e_shoff;
+    uint32_t e_flags;
+    uint16_t e_ehsize;
+    uint16_t e_phentsize;
+    uint16_t e_phnum;
+    uint16_t e_shentsize;
+    uint16_t e_shnum;
+    uint16_t e_shstrndx;
+};
+
+void readLittleEndian(uint8_t*,uint8_t *,int);
+void readLittleEndian(uint16_t*,uint8_t *,int);
+void readLittleEndian(uint32_t*,uint8_t *,int);
+void readLittleEndian(uint64_t*,uint8_t *,int);
+struct e_ident readElfIdent(ifstream&);
+void readElf32Header(ifstream&);
+e64_header readElf64Header(ifstream&);
+string readFile(string);
